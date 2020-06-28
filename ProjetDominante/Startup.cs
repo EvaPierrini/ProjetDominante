5c4f891bb73aa1d6655d07f4835d7f043dca9749
EvaPierrini <eva.pierrini@viacesi.fr>
2020-06-28 22:15:28 +0200

    ADDED: Server config

--- ProjetDominante/Startup.cs
+++ ProjetDominante/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjetDominante
 {
@@ -24,6 +25,24 @@ namespace ProjetDominante
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            // Database connection string.
+            // Make sure to update the Password value below from "Your_password123" to your actual password.
+            var connection = @"Server=db;Database=master;User=sa;Password=P@ssw0rd;";
+
+            // This line uses 'UseSqlServer' in the 'options' parameter
+            // with the connection string defined above.
+            //services.AddDbContext<ApplicationDbContext>(
+            //    options => options.UseSqlServer(connection));
+
+            //services.AddIdentity<ApplicationUser, IdentityRole>()
+            //    .AddEntityFrameworkStores<ApplicationDbContext>()
+            //    .AddDefaultTokenProviders();
+
+            services.AddMvc();
+
+            // Add application services.
+            //services.AddTransient<IEmailSender, AuthMessageSender>();
+            //services.AddTransient<ISmsSender, AuthMessageSender>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
